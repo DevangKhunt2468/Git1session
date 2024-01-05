e84774943e47b2c4245a222949c22774eb418347
DevangKhunt2468 <devangkhunt2468@gmail.com>
2024-01-05 16:19:50 +0530

    even-odd

---
+++ problem1.js
@@ -0,0 +1,10 @@
+// if the remainder is 0 we can say number is even else number is odd
+
+let number = 4;
+if (number % 2 == 0) {
+  console.log("even number");
+  
+} else{
+
+  console.log("odd number");
+}
